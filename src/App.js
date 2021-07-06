0e771fff3144b64825277e59ffbd929b6d428ac5
Juan David Castro <me@juandc.co>
2021-07-06 11:08:24 -0500

    Clase 03 en orden de grabación

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Edita <code>src/App.js</code> y guarda para recargar.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Apremnde 
         </a>
       </header>
     </div>
