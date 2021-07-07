58bcc261e4f07f9ad0ae2cffa61271f48f5df8fc
Juan David Castro <me@juandc.co>
2021-07-07 11:37:19 -0500

    Clase 10 en orden de grabación

--- src/App.js
+++ src/App.js
@@ -31,6 +31,20 @@ function App() {
       return todoText.includes(searchText);
     });
   }
+
+  const completeTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos[todoIndex].completed = true;
+    setTodos(newTodos);
+  };
+
+  const deleteTodo = (text) => {
+    const todoIndex = todos.findIndex(todo => todo.text === text);
+    const newTodos = [...todos];
+    newTodos.splice(todoIndex, 1);
+    setTodos(newTodos);
+  };
   
   return (
     <React.Fragment>
@@ -49,6 +63,8 @@ function App() {
             key={todo.text}
             text={todo.text}
             completed={todo.completed}
+            onComplete={() => completeTodo(todo.text)}
+            onDelete={() => deleteTodo(todo.text)}
           />
         ))}
       </TodoList>
--- src/TodoItem.js
+++ src/TodoItem.js
@@ -2,18 +2,11 @@ import React from 'react';
 import './TodoItem.css';
 
 function TodoItem(props) {
-  const onComplete = () => {
-    alert('Ya completaste el todo ' + props.text);
-  };
-  const onDelete = () => {
-    alert('Borraste el todo ' + props.text);
-  };
-  
   return (
     <li className="TodoItem">
       <span
         className={`Icon Icon-check ${props.completed && 'Icon-check--active'}`}
-        onClick={onComplete}
+        onClick={props.onComplete}
       >
         √
       </span>
@@ -22,7 +15,7 @@ function TodoItem(props) {
       </p>
       <span
         className="Icon Icon-delete"
-        onClick={onDelete}
+        onClick={props.onDelete}
       >
         X
       </span>
