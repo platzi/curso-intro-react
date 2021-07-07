5de553e322e0511497bbd0c7743ee2d23b36e6d2
Juan David Castro <me@juandc.co>
2021-07-07 09:59:00 -0500

    Clase 08 en orden de grabación

--- src/TodoSearch.js
+++ src/TodoSearch.js
@@ -2,17 +2,22 @@ import React from 'react';
 import './TodoSearch.css';
 
 function TodoSearch() {
+  const [searchValue, setSearchValue] = React.useState('');
+  
   const onSearchValueChange = (event) => {
     console.log(event.target.value);
+    setSearchValue(event.target.value);
   };
-  
-  return (
+
+  return [
     <input
       className="TodoSearch"
       placeholder="Cebolla"
+      value={searchValue}
       onChange={onSearchValueChange}
-    />
-  );
+    />,
+    <p>{searchValue}</p>
+  ];
 }
 
 export { TodoSearch };
